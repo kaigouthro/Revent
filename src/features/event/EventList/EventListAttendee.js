8c3a397fcb75bf6bce5f0d5ee308665bad511fca
TrungDT <thanhtrung.it25@gmail.com>
2018-11-18 16:36:00 +0700

    Structure file based on features

---
+++ src/features/event/EventList/EventListAttendee.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { List, Image } from "semantic-ui-react"
+
+const EventListAttendee = ({ attendee }) => (
+	<List>
+		<List.Item>
+			<Image as="a" circular src={attendee.photoURL} />
+		</List.Item>
+	</List>
+)
+
+export default EventListAttendee
---
+++ src/features/event/EventList/EventListItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import { Segment, Item, Icon, Button, List } from "semantic-ui-react"
+
+import EventListAttendee from "./EventListAttendee"
+
+const EventListItem = ({ event }) => {
+	const { title, date, description, venue, hostPhotoURL, hostedBy, attendees } = event
+
+	renderAttendees = () => attendees.map((attendee) => <EventListAttendee key={attendee.id} attendee={attendee} />)
+
+	return (
+		<Segment.Group>
+			<Segment>
+				<Item.Group>
+					<Item>
+						<Item.Image size="tiny" circular src={hostPhotoURL} />
+						<Item.Content>
+							<Item.Header as="a">Header</Item.Header>
+							<Item.Description>
+								Hosted by <a>{hostedBy}</a>
+							</Item.Description>
+						</Item.Content>
+					</Item>
+				</Item.Group>
+			</Segment>
+			<Segment>
+				<span>
+					<Icon name="clock" /> {date} |
+					<Icon name="marker" /> {venue}
+				</span>
+			</Segment>
+			<Segment secondary>
+				<List horizontal>{renderAttendees()}</List>
+			</Segment>
+			<Segment clearing>
+				<span>{description}</span>
+				<Button as="a" color="teal" floated="right" content="View" />
+			</Segment>
+		</Segment.Group>
+	)
+}
+
+export default EventListItem
