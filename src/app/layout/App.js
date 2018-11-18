948364d4d5edf8bba1d579a15916d15d756fe739
TrungDT <thanhtrung.it25@gmail.com>
2018-11-18 22:06:27 +0700

    Add NavBar to App layout

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react"
 import { Container } from "semantic-ui-react"
-import EventDashboard from "../../features/event/EventDashboard"
+import NavBar from "../../features/nav/NavBar"
+import EventDashboard from "../../features/events/EventDashboard"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Container className="main">
-          <h1>Re-vents</h1>
+          <NavBar />
           <EventDashboard />
         </Container>
       </div>
