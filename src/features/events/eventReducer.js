a3a25aff467ed16a5a955229a26f80980d7be07b
TrungDT <thanhtrung.it25@gmail.com>
2018-11-19 10:42:37 +0700

    Implementation of Redux state management for events feature

---
+++ src/features/events/eventReducer.js
@@ -0,0 +1,21 @@
+import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventContants"
+import { createReducer } from "../../app/common/utils/reducerUtil"
+import initialState from "./eventData"
+
+const createEvent = (state, payload) => [
+  ...state,
+  Object.assign({}, payload.event)
+]
+
+const updateEvent = (state, payload) => [
+  ...state.filter(event => event.id !== payload.event.id),
+  Object.assign({}, payload.event)
+]
+
+const deleteEvent = (state, payload) => [
+  ...state.filter(event => event.id !== payload.eventId)
+]
+
+export default createReducer(initialState, {
+  [CREATE_EVENT]: createEvent()
+})
