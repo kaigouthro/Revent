bdcdfe87b797536e104fe2bc28e291005e4391dc
TrungDT <thanhtrung.it25@gmail.com>
2018-11-20 20:35:20 +0700

    Activate loading spinner

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -4,13 +4,15 @@ import { reducer as formReducer } from "redux-form"
 import eventReducer from "../../features/events/eventReducer"
 import modalReducer from "../../features/modals/modalReducer"
 import authReducer from "../../features/auth/authReducer"
+import asyncReducer from "../../features/async/asyncReducer"
 
 const rootReducer = combineReducers({
   // test: testReducer,
   form: formReducer,
   events: eventReducer,
   modals: modalReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 })
 
 export default rootReducer
