0958310bad5e01dc7c8dae6541fa49e276d01034
TrungDT <thanhtrung.it25@gmail.com>
2018-11-25 17:47:13 +0700

    Change link loggedin menu

--- src/features/nav/Menus/LoggedInMenu.js
+++ src/features/nav/Menus/LoggedInMenu.js
@@ -7,7 +7,12 @@ const LoggedInMenu = ({ signOut, profile, auth }) => (
     <Image src={profile.photoURL || "/assets/user.png"} spaced="right" avatar />
     <Dropdown pointing="top left" text={profile.displayName}>
       <Dropdown.Menu>
-        <Dropdown.Item text="Create Event" icon="plus" />
+        <Dropdown.Item
+          as={Link}
+          to={`/createEvent`}
+          text="Create Event"
+          icon="plus"
+        />
         <Dropdown.Item text="My Events" icon="calendar" />
         <Dropdown.Item text="My Network" icon="users" />
         <Dropdown.Item
