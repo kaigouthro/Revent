0a32c23d7888cb53da2a4a47618b3a6b97a18a59
TrungDT <thanhtrung.it25@gmail.com>
2018-11-30 16:46:10 +0700

    Add UnauthModal to open when access route need authenticated

--- src/features/auth/authWrapper.js
+++ src/features/auth/authWrapper.js
@@ -1,11 +1,11 @@
-import { connectedRouterRedirect } from "redux-auth-wrapper/history4/redirect"
+import { connectedReduxRedirect } from "redux-auth-wrapper/history4/redirect"
 import { openModal } from "../modals/modalActions"
 
-export const AuthenticatedUser = connectedRouterRedirect({
+export const AuthenticatedUser = connectedReduxRedirect({
   redirectPath: "/events",
   allowRedirectBack: true,
   authenticatedSelector: ({ firebase: { auth } }) =>
-    auth.isLoad && !auth.isEmpty,
+    auth.isLoaded && !auth.isEmpty,
   wrapperDisplayName: "AuthenticatedUser",
   redirectAction: newLocation => dispatch => dispatch(openModal("UnauthModal"))
 })
